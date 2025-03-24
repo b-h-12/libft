be7b95f75a0882c11707406bae94c073808e8b5f
b-h-12 <bilal.hamoum@hotmail.fr>
2025-03-24 15:16:52 +0100

    Added str_1 functions

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bhamoum <bhamoum@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 12:32:34 by bhamoum           #+#    #+#             */
-/*   Updated: 2025/03/24 14:38:37 by bhamoum          ###   ########.fr       */
+/*   Updated: 2025/03/24 15:14:55 by bhamoum          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,17 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
+typedef unsigned long size_t;
+
 int	isalpha(int c);
 int	isdigit(int c);
 int	isalnum(int c);
 int	isascii(int c);
 int	isprint(int c);
+int	strlen(const char *s);
+int	strlcpy(char *dst, const char *src, size_t size);
+int	strlcat(char *dst, const char *src, size_t size);
+int	strncmp(const char *s1, const char *s2, size_t n);
+char		*strnstr(const char *big, const char *little, size_t len);
 
 #endif
--- test.c
+++ test.c
@@ -6,7 +6,25 @@
 /*   By: bhamoum <bhamoum@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 12:42:02 by bhamoum           #+#    #+#             */
-/*   Updated: 2025/03/24 12:43:15 by bhamoum          ###   ########.fr       */
+/*   Updated: 2025/03/24 15:07:04 by bhamoum          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include "libft.h"
+
+int main()
+{
+	//test strlcopy
+	char src[] = "Hello";
+	char dest[10];
+	strlcpy(dest, src, 5);
+	printf("%s\n", dest);
+	//test strlcat
+	char src1[] = "Hello";
+	char dest1[10] = "World";
+	strlcat(dest1, src1, 9);
+	printf("%s\n", dest1);
+	
+	return (0);
+}
