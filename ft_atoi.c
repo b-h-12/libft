933ec930bf16ad441112ad7c5b4732d3451203a4
b-h-12 <bilal.hamoum@hotmail.fr>
2025-03-31 17:09:40 +0200

    Add ft_strcmp function and update ft_atoi to use it for string comparison

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,13 +6,23 @@
 /*   By: bhamoum <bhamoum@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 12:43:47 by bhamoum           #+#    #+#             */
-/*   Updated: 2025/03/31 12:47:03 by bhamoum          ###   ########.fr       */
+/*   Updated: 2025/03/31 16:16:16 by bhamoum          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_atoi(char *str)
+static int	ft_strcmp(const char *s1, const char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+}
+
+int	ft_atoi(const char *str)
 {
 	int	i;
 	int	sign;
@@ -24,7 +34,7 @@ int	ft_atoi(char *str)
 	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v'
 		|| str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
 		i++;
-	if (strcmp(str, "-2147483648") == 0)
+	if (ft_strcmp(str, "-2147483648") == 0)
 		return (-2147483648);
 	while (str[i] == '-' || str[i] == '+')
 	{
