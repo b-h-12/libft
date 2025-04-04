a32c4940b4e9298bfc2036d9339ba445b2a387ac
b-h-12 <bilal.hamoum@hotmail.fr>
2025-04-04 19:17:08 +0200

    Add null check for set in ft_strtrim and refactor to use ft_substr for substring extraction

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: bhamoum <bhamoum@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 14:09:39 by bhamoum           #+#    #+#             */
-/*   Updated: 2025/03/31 17:20:05 by bhamoum          ###   ########.fr       */
+/*   Updated: 2025/04/04 19:16:57 by bhamoum          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,16 @@ char	*ft_strtrim(char const *s1, char const *set)
 	size_t	start;
 	size_t	end;
 
-	if (!s1 || !set)
+	if (!s1)
 		return (NULL);
+	if (!set)
+		return (ft_strdup(s1));
 	start = 0;
 	while (s1[start] && ft_strchr(set, s1[start]))
 		start++;
 	end = ft_strlen(s1);
 	while (end > start && ft_strchr(set, s1[end - 1]))
 		end--;
-	str = (char *)malloc(end - start + 1);
-	if (!str)
-		return (NULL);
-	ft_strlcpy(str, &s1[start], end - start);
-	str[end - start] = '\0';
+	str = ft_substr(s1, start, end - start);
 	return (str);
 }
