7e54606191738d3fb6088633675e1a7a117ab742
b-h-12 <bilal.hamoum@hotmail.fr>
2025-04-04 19:02:17 +0200

    Add null check in ft_strlen to handle null pointer input

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: bhamoum <bhamoum@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 13:14:45 by bhamoum           #+#    #+#             */
-/*   Updated: 2025/03/31 17:18:47 by bhamoum          ###   ########.fr       */
+/*   Updated: 2025/04/04 19:02:07 by bhamoum          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ size_t	ft_strlen(const char *s)
 	size_t	i;
 
 	i = 0;
+	if (!s)
+		return (0);
 	while (s[i])
 		i++;
 	return (i);
