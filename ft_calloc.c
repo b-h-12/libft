df41594c2fba6e3c45360a90589a9663045e4076
b-h-12 <bilal.hamoum@hotmail.fr>
2025-04-14 18:00:16 +0200

    Update ft_calloc to check for int overflow in nmemb and size parameters

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: bhamoum <bhamoum@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 13:06:15 by bhamoum           #+#    #+#             */
-/*   Updated: 2025/04/14 16:03:24 by bhamoum          ###   ########.fr       */
+/*   Updated: 2025/04/14 18:00:09 by bhamoum          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
 	
-	if (nmemb * size > INT_MAX)
+	if (nmemb * size > INT_MAX || nmemb > INT_MAX || size > INT_MAX)
 		return (NULL);
 	ptr = malloc(nmemb * size);
 	if (!ptr)
